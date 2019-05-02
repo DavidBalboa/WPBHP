64843e4c1431c12c528836c5ef13670be9f491be
DavidBalboa <33736411+DavidBalboa@users.noreply.github.com>
2019-05-02 19:41:34 -0400

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define('DB_USER', 'wordpressuser2');
 define('DB_PASSWORD', 'password');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', '10.128.1.102');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
